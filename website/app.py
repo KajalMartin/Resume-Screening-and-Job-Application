82df6dbdf5488624fbb9454529ff819cc2c9f1b9
KajalMartin <kajalmartin99@gmail.com>
2025-12-29 00:24:10 +0530

    Test folders website and live-website created, home page completed

---
+++ website/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home(): 
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
